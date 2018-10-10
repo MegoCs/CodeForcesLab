d1ebe432fb1f05317e6a3bb1aa0662b5ac54339a
Ahmed Kamel <Ahmed.Kamel@linkdev.com>
2018-10-10 14:03:15 +0200

    69A - Young Physicist

--- CodeForcesLab/Program.cs
+++ CodeForcesLab/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 
 namespace CodeForcesLab
 {
@@ -6,7 +7,26 @@ namespace CodeForcesLab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int Forces = int.Parse(Console.ReadLine());
+            ObjectForces obj = new ObjectForces();
+            for (int i = 0; i < Forces; i++)
+            {
+                string strLine = Console.ReadLine();
+                var vectorNums = strLine.Split(' ');
+                obj.XForce += double.Parse(vectorNums[0]);
+                obj.YForce += double.Parse(vectorNums[1]);
+                obj.ZForce += double.Parse(vectorNums[2]);
+            }
+            if (obj.XForce == 0 && obj.YForce == 0 && obj.ZForce == 0)
+                Console.WriteLine("YES");
+            else
+                Console.WriteLine("NO");
+        }
+        class ObjectForces
+        {
+            public double XForce { get; set; }
+            public double YForce { get; set; }
+            public double ZForce { get; set; }
         }
     }
 }
